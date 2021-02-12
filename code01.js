b9dbc130fffe85527cc234b7adaa86eac993cd04
euzebiosantana.github <63376480+euzebiosantanawork@users.noreply.github.com>
2021-02-12 06:19:40 -0300

    Create code01.js

---
+++ code01.js
@@ -0,0 +1,23 @@
+import types from './actionTypes'
+
+let hideTimeout
+
+const clouseToast = () => {
+    clearTimeout (hideTimeout)
+    retrun {type: types.CLOSE_TOAST,payloand:''}
+}
+
+const notify = message => {
+    
+    return  dispatch =>{
+
+        clearTimeout(hideTimeout)
+        hideTimeout = setTimeout(dispatch.bind({}, closeToast(), 7000)
+
+        dispatch ({types: types.OPEN_TOAST, payloand: {message} } )
+    }
+}
+
+export {
+    closeToast
+}
