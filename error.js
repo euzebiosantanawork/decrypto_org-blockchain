bbedc3b6d37fea61765e4cd9377f7cafa3eb7ca7
euzebiosantana.github <63376480+euzebiosantanawork@users.noreply.github.com>
2021-02-18 06:33:50 -0300

    Create error.js

---
+++ error.js
@@ -0,0 +1,22 @@
+const {HTTPError} = require ('../errors')
+
+const HTTPErrorHandler = ( err , req , res , next ) => {
+
+    if ( err instanceof HTTPError ) {
+        
+        return res.status(err.statusCode).json({
+            error:{
+                code: err.statusCode,
+                message: err.message
+            }
+        })
+
+    }
+
+    next (err.message)
+
+}
+
+module.export = {
+    HTTPErrorHandler
+}
