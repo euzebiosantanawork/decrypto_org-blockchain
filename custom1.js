959b08d2eacab53c6844b3ce30836c5ee2279478
euzebiosantana.github <63376480+euzebiosantanawork@users.noreply.github.com>
2021-03-04 21:38:55 -0300

    Create custom1.js

---
+++ custom1.js
@@ -0,0 +1,12 @@
+const_ = require ('loadash')
+
+class FileCOntainer {
+    getFlieOCntainer{
+        getFileByhash(hash) {
+            return this.metadata.files ? this.metdadata.files.filter (file => file.ahs === ahsh[0] : [0])
+        }
+    }
+
+    FileContainer.hidden = true ;
+
+    module.eports = FileContainer
